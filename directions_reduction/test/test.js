0bdbc5959f3cc46728abea8dc0b0b695eb06d005
Leigh Mathieson <leigh.mathieson@gmail.com>
2018-08-09 16:49:10 +0100

    Updating solution

--- directions_reduction/test/test.js
+++ directions_reduction/test/test.js
@@ -2,20 +2,20 @@
 /* eslint-disable prefer-arrow-callback, func-names */
 
 const { expect } = require('chai');
-const isSolved = require('../checker.js');
+const dirReduc = require('../directions_reduction.js');
 
-describe('Checks if tic-tac-toe game is complete - calling isSolved', function () {
+describe('Removes opposing directions', function () {
   const tests = [
-    { args: [[2, 1, 2], [2, 1, 1], [1, 2, 1]], expected: 0 },
-    { args: [[2, 2, 2], [0, 1, 1], [1, 0, 0]], expected: 2 },
-    { args: [[1, 2, 1], [1, 1, 2], [2, 1, 2]], expected: 0 },
-    { args: [[2, 1, 1], [0, 1, 1], [2, 2, 2]], expected: -1 },
-    { args: [[1, 1, 1], [0, 2, 2], [0, 0, 0]], expected: 1 },
+    { args: ['NORTH', 'SOUTH', 'SOUTH', 'EAST', 'WEST', 'NORTH', 'WEST'], expected: ['WEST'] },
+    { args: ['NORTH', 'WEST', 'SOUTH', 'EAST'], expected: ['NORTH', 'WEST', 'SOUTH', 'EAST'] },
+    { args: ['NORTH', 'SOUTH', 'EAST', 'WEST', 'EAST', 'WEST'], expected: [] },
   ];
   tests.forEach(function (test) {
-    it(`Checks [${test.args[0]}], [${test.args[1]}], [${test.args[2]}] and returns the winner of the game as ${test.expected}`, function () {
-      const res = isSolved(test.args);
+    it(`Checks ${test.args} resolves to ${test.expected}`, function () {
+      const res = dirReduc(test.args);
       expect(res).to.eql(test.expected);
     });
   });
 });
+
+
--- doubleColonBind.js
+++
@@ -1,7 +0,0 @@
-Promise.resolve(123).then(::console.log.bind());
-
-/*
-(...args) => this.handleStuff('stuff', ...args);
-
-this.handleStuff.bind(this, 'stuff');
-*/
