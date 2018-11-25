56237123a8a3a9379fb25c2bcd0b3caa00d7ca61
John Berthels <john.berthels@gmail.com>
2018-11-25 09:26:00 +0000

    Refactor - variable naming and termination
    Avoid the use of a two-element array for the max.
    
    Move the termination check out of the loop and place
    afterwards.

--- 6_kyu/longest_consec/longestConsec.js
+++ 6_kyu/longest_consec/longestConsec.js
@@ -9,40 +9,39 @@ n being the length of the string array, if n = 0 or k > n or k <= 0 return ""
 
 // Finds the longest k consecutive string concatenated, from input array containing n entries
 
-function longestConsec(strarr, k) {
-  const n = strarr.length;
+function longestConsec(strings, k) {
+  const n = strings.length;
+ 
+  // Early return for trivial cases
   if ((n === 0) || (k > n) || (k <= 0)) {
     return '';
   }
 
-  // Pseudo: countPointer is [`longest concat character count`, `starting index in strarr`]
-  const countPointer = [0, 0];
-  let tempCount = 0;
+  let longestCount = 0;
+  let longestCountIndex = 0;
+
+  let currentCount = 0;
 
   // start at beginning of array, loop to n-k, sum the elements concat with next k items in array
   for (let i = 0; i <= (n - k); i++) {
-    tempCount = 0;
+    currentCount = 0;
     for (let j = i; j < i + k; j++) {
-      tempCount += strarr[j].length;
+      currentCount += strings[j].length;
     }
-    // if larger than current max, replace with current max and store position in countPointer[1]
-    if (tempCount > countPointer[0]) {
-      countPointer[0] = tempCount;
-      countPointer[1] = i;
-    }
-    // when reach end, cycle through array from given starting element and return concat. strings
-    if (i === (n - k)) {
-      let str = '';
-      for (let l = countPointer[1]; l < (countPointer[1] + k); l++) {
-        str += strarr[l];
-      }
-      return str;
+    if (currentCount > longestCount) {
+      longestCount = currentCount;
+      longestCountIndex = i;
     }
   }
-  return '';
+
+  let str = '';
+  for (let l = longestCountIndex; l < (longestCountIndex + k); l++) {
+    str += strings[l];
+  }
+  return str;
 }
 
-// console.log(longestConsec(['zone', 'abignale', 'theta', 'libeboo', 'thetrtya', 'abigailg'], 4));
+console.log(longestConsec(['zone', 'abignale', 'theta', 'libeboo', 'thetrtya', 'abigailg'], 4));
 
 // for test
 module.exports = longestConsec;
